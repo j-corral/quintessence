424c2b738d059b40bf183c4fc33a8b085b4edaa3
Jonathan Corral <johnlejardinnier@gmail.com>
2016-04-17 17:45:23 +0200

    fixed config

---
+++ app/config/usersDB/users.config.sample.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 04/03/16
+ * Time: 17:22
+ */
+
+/**
+ * Fichier config
+ *
+ * Liste des utilisateurs SQL
+ */
+return array(
+
+	"db_users" => [
+
+		// User SQL
+		//"user" => "password",
+
+		
+	]
+
+);
